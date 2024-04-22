db2fd13b6b42b431ac9180c8468f35d5286ca3ca
Nazarii Huliak <96128629+NazariiHuliak@users.noreply.github.com>
2024-04-22 22:51:19 +0300

    Created pages: Sign In Sing Up

---
+++ Project/app/src/main/java/com/example/timeandtune/Presentation/Authentication/SignIn.kt
@@ -0,0 +1,12 @@
+package com.example.timeandtune.Presentation.Authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.timeandtune.R
+
+class SignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_in_layout)
+    }
+}
---
+++ Project/app/src/main/java/com/example/timeandtune/Presentation/Authentication/SignUp.kt
@@ -0,0 +1,12 @@
+package com.example.timeandtune.Presentation.Authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.timeandtune.R
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up_layout)
+    }
+}
